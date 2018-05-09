6b000d164eee73c14208f9a8bc27fcdca264797d
白柳明香里 <asm13.5taykox@gmail.com>
2018-05-09 11:01:58 +0900

    comment   model,controllerを作成。アソシエーション定義。 prototype#show_viewでrenderさせたけどコメント欄が表示されない！

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
 
-  resources :prototypes, only: [:index, :new, :create, :show]
   resources :users, only: [:show, :edit, :update]
+  resources :prototypes, only: [:index, :new, :create, :show] do
+    resources :comments, only: [:create]
+  end
+
 end
