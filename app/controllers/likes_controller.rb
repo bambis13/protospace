a9de53d7a806424c89a83d916511638fae4099f4
関川智 <t.g.i.f.0312@gmail.com>
2018-05-10 10:25:13 +0900

    add_render_to_prototype-show

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,8 +6,8 @@ class LikesController < ApplicationController
   end
 
   def destroy
-    like = Like.find_by(user_id: current_user.id, prototype_id: params[:prototype_id])
-    like.destroy
+    @like = Like.find_by(user_id: current_user.id, prototype_id: params[:prototype_id])
+    @like.destroy
     @likes = Like.where(prototype_id: params[:prototype_id])
     @prototypes = Prototype.all
   end
--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -20,6 +20,7 @@ class PrototypesController < ApplicationController
   end
 
   def show
+    @like = Like.find_by(user_id: current_user.id, prototype_id: params[:id])
   end
 
   private
