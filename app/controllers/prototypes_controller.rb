1e0bb8f33e8834454360190c44fbde8a438693b8
yousukekaneko <kaneko1987-0402@i.softbank.jp>
2018-05-14 11:17:47 +0900

    [FIXME] コメントの表示機能追加
    プロトタイプのshowアクションに、
    @comments を追加した。
    Viewファイルに、
    コメント一覧表を追加
    [FIX ME]comments/_indexのviewが崩れている

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -21,6 +21,7 @@ class PrototypesController < ApplicationController
 
   def show
     @comment = Comment.new
+    @comments = Comment.where(prototype_id: params[:id]).includes(:user)
   end
 
   private
