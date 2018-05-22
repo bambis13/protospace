3f85bb94be6c0d15d2566bc8a70ebf997c0604dc
白柳明香里 <asm13.5taykox@gmail.com>
2018-05-22 14:50:00 +0900

    remove old version

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180509003501) do
 ActiveRecord::Schema.define(version: 20180515003338) do
 
   create_table "captured_images", force: :cascade do |t|
