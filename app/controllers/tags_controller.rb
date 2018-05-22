3c896ac565eea01ab3e313192d416fd89b601eac
関川智 <t.g.i.f.0312@gmail.com>
2018-05-22 11:12:19 +0900

    indexアクションの定義

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,10 @@
 class TagsController < ApplicationController
+  def index
+    @tags = Tag.all
+  end
+
+  # def show
+  #   @tag = Tag.find(params[:id])
+  #   @prototypes = Prototype.where(prototype_id: params[:id]).include
+  # end
 end
