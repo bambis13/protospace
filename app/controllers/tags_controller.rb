febf83bb4151423c187da40d978ff24d8d985dc1
白柳明香里 <asm13.5taykox@gmail.com>
2018-05-24 09:21:03 +0900

    tag#indexとtag#showにページネーション装備

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,10 +1,10 @@
 class TagsController < ApplicationController
   def index
-    @tags = Tag.all
+    @tags = Tag.all.page(params[:page]).per(20)
   end
 
   def show
     @tag = Tag.find(params[:id])
-    @prototypes = @tag.prototypes
+    @prototypes = @tag.prototypes.page(params[:page]).per(10)
   end
 end
